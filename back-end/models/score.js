07e98ee03d16d3368e825689cbeb3a42a5410ad3
Haddock <jhadddock@protonmail.com>
2024-02-01 14:42:20 -0800

    Change score to time

--- back-end/models/score.js
+++ back-end/models/score.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 
 const ScoreSchema = new Schema({
   player_name: { type: String, required: true, maxLength: 100 },
-  score: { type: Number, required: true },
+  time: { type: Number, required: true },
 });
 
-ScoreSchema.virtual("time").get(function () {
-  return this.score;
+ScoreSchema.virtual("dsplay_time").get(function () {
+  return this.time;
 });
 
 module.exports = mongoose.Model("Score", ScoreSchema);
