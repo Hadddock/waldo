a01a0c49ba3b8d89ac8dff2edcc42634c6a02b37
Justin Haddock <jhadddock@protonmail.com>
2024-02-06 09:22:08 -0800

    Fix display_time

--- back-end/models/score.js
+++ back-end/models/score.js
@@ -11,13 +11,12 @@ const ScoreSchema = new Schema({
 ScoreSchema.virtual("display_time").get(function () {
   const ms = this.time;
 
-  const remainingMilliseconds = ms % 1000;
-  const totalSeconds = Math.round(ms / 1000);
+  const totalSeconds = ms / 1000;
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds - hours * 3600) / 60);
   const seconds = totalSeconds - hours * 3600 - minutes * 60;
 
-  return `${minutes}:${seconds}.${remainingMilliseconds}`;
+  return `${hours}:${minutes}:${seconds}`;
 });
 
 module.exports = mongoose.model("Score", ScoreSchema);
